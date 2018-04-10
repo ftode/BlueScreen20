1ec21c3c1f6ca341d5ed5f903f3ff20a7a675474
Frank Tottleben <ftottleb@ent.clariant.com>
2018-04-10 16:25:03 +0200

    Initial commit

---
+++ app/src/main/java/de/ftottleb/bluescreen20/FullscreenActivity.java
@@ -0,0 +1,315 @@
+package de.ftottleb.bluescreen20;
+
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.provider.Settings;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class FullscreenActivity extends AppCompatActivity {
+    /**
+     * Whether or not the system UI should be auto-hidden after
+     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+     */
+    private static final boolean AUTO_HIDE = true;
+
+    /**
+     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+     * user interaction before hiding the system UI.
+     */
+    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+    /**
+     * Some older devices needs a small delay between UI widget updates
+     * and a change of the status and navigation bar.
+     */
+    private static final int UI_ANIMATION_DELAY = 300;
+    private final Handler mHideHandler = new Handler();
+    private TextView mContentView;
+    private TextView mBrightTextView;
+    private int level;
+    private String hours;
+    private String minutes;
+    private static BroadcastReceiver tickReceiver;
+    private Context mContext;
+    private SeekBar mSeekBar;
+
+    private final Runnable mHidePart2Runnable = new Runnable() {
+        @SuppressLint("InlinedApi")
+        @Override
+        public void run() {
+            // Delayed removal of status and navigation bar
+
+            // Note that some of these constants are new as of API 16 (Jelly Bean)
+            // and API 19 (KitKat). It is safe to use them, as they are inlined
+            // at compile-time and do nothing on earlier devices.
+            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+    };
+    private View mControlsView;
+    private final Runnable mShowPart2Runnable = new Runnable() {
+        @Override
+        public void run() {
+            // Delayed display of UI elements
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.show();
+            }
+            mControlsView.setVisibility(View.VISIBLE);
+        }
+    };
+    private boolean mVisible;
+    private final Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            hide();
+        }
+    };
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (AUTO_HIDE) {
+                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen);
+        // Get the application context
+        mContext = getApplicationContext();
+        mVisible = true;
+        mControlsView = findViewById(R.id.fullscreen_content_controls);
+        mContentView = findViewById(R.id.fullscreen_content);
+        mBrightTextView = findViewById(R.id.textViewBrightness);
+        mSeekBar = (SeekBar) findViewById(R.id.seekBar);
+        this.registerReceiver(this.mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+        // Set up the user interaction to manually show or hide the system UI.
+        mContentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggle();
+            }
+        });
+        // Set the SeekBar initial progress from screen current brightness
+        int brightness = getScreenBrightness();
+        mSeekBar.setProgress(brightness);
+        // Set a SeekBar change listener
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                // Change the screen brightness
+                setScreenBrightness(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        // output battery level & time
+        hours = String.valueOf(Calendar.getInstance().get(Calendar.HOUR_OF_DAY));
+        minutes = String.valueOf(Calendar.getInstance().get(Calendar.MINUTE));
+        mContentView.setText((String.valueOf(level) + "%\n"));
+        mContentView.append(hours+":"+minutes);
+        //Create a broadcast receiver to handle change in time
+        tickReceiver=new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if(intent.getAction().compareTo(Intent.ACTION_TIME_TICK)==0)             {
+                    hours = String.valueOf(Calendar.getInstance().get(Calendar.HOUR_OF_DAY));
+                    minutes = String.valueOf(Calendar.getInstance().get(Calendar.MINUTE));
+                    mContentView.setText((String.valueOf(level) + "%\n"));
+                    mContentView.append(hours+":"+minutes);}
+            }
+        };
+//Register the broadcast receiver to receive TIME_TICK
+        registerReceiver(tickReceiver, new IntentFilter(Intent.ACTION_TIME_TICK));
+
+        // Upon interacting with UI controls, delay any scheduled hide()
+        // operations to prevent the jarring behavior of controls going away
+        // while interacting with the UI.
+     //   findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100);
+    }
+
+    private void toggle() {
+        if (mVisible) {
+            hide();
+        } else {
+            show();
+        }
+    }
+
+    private void hide() {
+        // Hide UI first
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        mControlsView.setVisibility(View.GONE);
+        mVisible = false;
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        mHideHandler.removeCallbacks(mShowPart2Runnable);
+        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+    }
+
+    @SuppressLint("InlinedApi")
+    private void show() {
+        // Show the system bar
+        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        mVisible = true;
+
+        // Schedule a runnable to display UI elements after a delay
+        mHideHandler.removeCallbacks(mHidePart2Runnable);
+        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
+    }
+
+    /**
+     * Schedules a call to hide() in delay milliseconds, canceling any
+     * previously scheduled calls.
+     */
+    private void delayedHide(int delayMillis) {
+        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+    // Broadcasts
+
+    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context ctxt, Intent intent) {
+            level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            hours = String.valueOf(Calendar.getInstance().get(Calendar.HOUR_OF_DAY));
+            minutes = String.valueOf(Calendar.getInstance().get(Calendar.MINUTE));
+            mContentView.setText((String.valueOf(level) + "%\n"));
+            mContentView.append(hours+":"+minutes);
+        }
+    };
+    // Get the screen current brightness
+    protected int getScreenBrightness(){
+        /*
+            public static int getInt (ContentResolver cr, String name, int def)
+                Convenience function for retrieving a single system settings value as an integer.
+                Note that internally setting values are always stored as strings; this function
+                converts the string to an integer for you. The default value will be returned
+                if the setting is not defined or not an integer.
+
+            Parameters
+                cr : The ContentResolver to access.
+                name : The name of the setting to retrieve.
+                def : Value to return if the setting is not defined.
+            Returns
+                The setting's current value, or 'def' if it is not defined or not a valid integer.
+        */
+        int brightnessValue = Settings.System.getInt(
+                mContext.getContentResolver(),
+                Settings.System.SCREEN_BRIGHTNESS,
+                0
+        );
+        return brightnessValue;
+    }
+    // Change the screen brightness
+    public void setScreenBrightness(int brightnessValue){
+        /*
+            public abstract ContentResolver getContentResolver ()
+                Return a ContentResolver instance for your application's package.
+        */
+        /*
+            Settings
+                The Settings provider contains global system-level device preferences.
+
+            Settings.System
+                System settings, containing miscellaneous system preferences. This table holds
+                simple name/value pairs. There are convenience functions for accessing
+                individual settings entries.
+        */
+        /*
+            public static final String SCREEN_BRIGHTNESS
+                The screen backlight brightness between 0 and 255.
+                Constant Value: "screen_brightness"
+        */
+        /*
+            public static boolean putInt (ContentResolver cr, String name, int value)
+                Convenience function for updating a single settings value as an integer. This will
+                either create a new entry in the table if the given name does not exist, or modify
+                the value of the existing row with that name. Note that internally setting values
+                are always stored as strings, so this function converts the given value to a
+                string before storing it.
+
+            Parameters
+                cr : The ContentResolver to access.
+                name : The name of the setting to modify.
+                value : The new value for the setting.
+            Returns
+                true : if the value was set, false on database errors
+        */
+
+        // Make sure brightness value between 0 to 255
+        if(brightnessValue >= 0 && brightnessValue <= 255){
+        // Check whether has the write settings permission or not.
+        boolean settingsCanWrite = Settings.System.canWrite(mContext);
+        if(!settingsCanWrite) {
+            // If do not have write settings permission then open the Can modify system settings panel.
+            Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+            startActivity(intent);
+        }
+        settingsCanWrite = Settings.System.canWrite(mContext);
+        if(settingsCanWrite){
+            Settings.System.putInt(
+                    mContext.getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS,brightnessValue);
+            mBrightTextView.setText(brightnessValue);}
+        }
+    }
+
+
+
+}
